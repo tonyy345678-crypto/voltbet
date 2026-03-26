830073e610f56b44faf996527c09b2a7485e00f0
tonyy345678-crypto <tonyy345678@gmail.com>
2026-03-27 01:25:39 +0300

    Add files via upload

--- TitanBet_Platform/admin_app.js
+++ TitanBet_Platform/admin_app.js
@@ -814,11 +814,11 @@ function initAdminPanel() {
         if(document.getElementById('nav-staff')) document.getElementById('nav-staff').style.display = 'none';
         if(document.getElementById('nav-users')) document.getElementById('nav-users').style.display = 'none';
         if(document.getElementById('nav-deposits')) document.getElementById('nav-deposits').style.display = 'block';
-        if(document.getElementById('nav-deposit-history')) document.getElementById('nav-deposit-history').style.display = 'none';
+        if(document.getElementById('nav-deposit-history')) document.getElementById('nav-deposit-history').style.display = 'block';
         if(document.getElementById('nav-withdraws')) document.getElementById('nav-withdraws').style.display = 'block';
-        if(document.getElementById('nav-withdraw-history')) document.getElementById('nav-withdraw-history').style.display = 'none';
+        if(document.getElementById('nav-withdraw-history')) document.getElementById('nav-withdraw-history').style.display = 'block';
         if(document.getElementById('nav-banks')) document.getElementById('nav-banks').style.display = 'block';
-        if(document.getElementById('nav-support')) document.getElementById('nav-support').style.display = 'block';
+        if(document.getElementById('nav-support')) document.getElementById('nav-support').style.display = 'none';
         if(document.getElementById('nav-settings')) document.getElementById('nav-settings').style.display = 'none';
         
         if(document.getElementById('top-kasa-widget')) document.getElementById('top-kasa-widget').style.display = 'none';
